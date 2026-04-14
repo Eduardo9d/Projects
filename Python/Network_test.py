2abaf74645f49ab42a3be7d93b411327a2d47e09
Eduardo9d <eduardo9d@gmail.com>
2026-04-14 17:26:37 +0100

    Upddate Network Test Script and Results

--- Python/Network_test.py
+++ Python/Network_test.py
@@ -22,7 +22,8 @@ def ping_host(ip):
 
 def scan_ports(ip, ports=None):
     if ports is None:
-        ports = [22, 80, 443, 3389]
+        # Common ports to scan
+        ports = [21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 993, 995, 3389]
     port_results = []
     for port in ports:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
